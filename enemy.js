4a171bcba96e0d6911dee790da0b1bf1da24f082
HYUKSKEE <gin280833@gmail.com>
2022-07-23 17:36:14 +0900

    07/23 ghost 생성중..

--- enemy.js
+++ enemy.js
@@ -1,35 +1,26 @@
-class Enemy {
-  constructor(name, top, left) {
-    this.name = name;
-    this.top = top;
-    this.left = left;
-  }
-  randomNumber() {
-    /* const randomGhost = quizs[Math.floor(Math.random() * quizs.length)]; */
-    return Math.random(this.left);
-  }
-
-  moveToBottom() {
-    const bottomPosition = getComputedStyle(element).bottom;
-    const regex = /[^0-9]/g;
-    const bottomPositionNum = bottomPosition.replace(regex, "");
-    let ghostNumber = parseInt(bottomPositionNum);
-    return AuthenticatorAssertionResponse;
-  }
-}
-
-const startButton = document.querySelector("#button");
+const startButton = document.querySelector(".start");
 const ghost = document.querySelector("#ghost-box");
-const parentDiv = document.querySelector("#bg");
-const ghostPositionValue = getComputedStyle(heroDiv).top;
+const bgDiv = document.querySelector("#bg");
+/* const ghostPositionValue = getComputedStyle(heroDiv).top; */
 /* const regex = /[^0-9]/g; */
-const ghostPositionNum = backgroundPositionValue.replace(regex, "");
-console.log(ghostPositionNum);
-let ghostNumber = parseInt(ghostPositionNum);
+/* const ghostPositionNum = backgroundPositionValue.replace(regex, ""); */
+/* console.log(ghostPositionNum); */
+/* let ghostNumber = parseInt(ghostPositionNum); */
 /* const a = setInterval(() => {
   console.log("hi");
 }, 3000); */
 
-function gameStart() {}
+function gameStart() {
+  setInterval(createRandomGhostDiv(), 3000);
+}
 
-/* startButton.addEventListener("click", gameStart); */
+function createRandomGhostDiv() {
+  const randomLeft = Math.random() * (780 - 20) + 20;
+  const newDiv = document.createElement("div");
+  const cleanDiv = bgDiv.appendChild(newDiv);
+  cleanDiv.id = "ghost-box";
+  cleanDiv.className = "ghost";
+  cleanDiv.style.cssText = `left: ${randomLeft}px`;
+  console.log("hi");
+}
+startButton.addEventListener("click", gameStart);
